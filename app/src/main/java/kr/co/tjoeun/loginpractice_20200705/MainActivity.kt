c2f17eb02644e174598683d4440f1a0744dc880d
황선혜 <ghkdtjs6451@gmail.com>
2020-07-05 11:28:28 +0900

    로그인 화면 흉내내기 마무리

--- app/src/main/java/kr/co/tjoeun/loginpractice_20200705/MainActivity.kt
+++ app/src/main/java/kr/co/tjoeun/loginpractice_20200705/MainActivity.kt
@@ -2,6 +2,7 @@ package kr.co.tjoeun.loginpractice_20200705
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -11,7 +12,13 @@ class MainActivity : AppCompatActivity() {
 
 //        버튼 눌렀을때
         loginBtn.setOnClickListener {
+            val email = emailEdt.toString()
+            val pw = pwdEdt.toString()
 
+//            if문으로 확인
+            if(email == "admin@test.com" && pw == "qwer1234"){
+                Toast.makeText(this, "관리자입니다.", Toast.LENGTH_SHORT).show()
+            }else{Toast.makeText(this, "로그인 실패.", Toast.LENGTH_SHORT).show()}
 
         }
     }
