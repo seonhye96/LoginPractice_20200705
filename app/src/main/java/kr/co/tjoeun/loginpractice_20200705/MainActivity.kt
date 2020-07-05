cfcdd30ae55578ce8b16b46ccb22827d7959f684
황선혜 <ghkdtjs6451@gmail.com>
2020-07-05 11:22:20 +0900

    Initial commit

---
+++ app/src/main/java/kr/co/tjoeun/loginpractice_20200705/MainActivity.kt
@@ -0,0 +1,18 @@
+package kr.co.tjoeun.loginpractice_20200705
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+//        버튼 눌렀을때
+        loginBtn.setOnClickListener {
+
+
+        }
+    }
+}
